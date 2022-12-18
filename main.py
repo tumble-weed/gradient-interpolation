87712582540f5b78bd1f530c38fb4308ae644810
tumble-weed <aniketsinghresearch@gmail.com>
2022-12-18 10:21:57 +0000

    main

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from sympy import Matrix,symbols,Symbol
 from IPython.display import display
 from sympy.solvers import solve
 import copy
-
-ndim = 2
-nthresh = 3
+from approximation import setup_approximations,get_approx_change_in_f
+from solve import setup_change_in_f_linear_equation,get_coeffs_for_linear_equation
 def setup(ndim,nthresh):
     """
     creates the symbols for s,t, fixed thresholds, observed_f
@@ -22,6 +21,9 @@ def setup(ndim,nthresh):
     gfixed = Matrix(symbols( [[f'g{i}__t{t}' for t in range(1,nthresh+1)] for i in range(1,ndim+1)]))
     M = Matrix(symbols(f'm1:{ndim+1}'))
     return x,g,s,t,ti,f_obs,M,Mfixed,gfixed
+#=============================
+ndim = 2
+nthresh = 3
 x,g,s,t,ti,f_obs,M,Mfixed,gfixed = setup(ndim,nthresh)
 a,b,c = setup_approximations(2,2)
 display(a,b,c)
@@ -36,6 +38,7 @@ lin_eq1 = setup_change_in_f_linear_equation(approx_change_in_f,
                                             gfixed[:,0],gfixed[:,1],
                                         a[:,0],b[:,0],c[:,0],
                                         M,Mfixed[:,0],Mfixed[:,1])
+get_coeffs_for_linear_equation(approx_change_in_f,ti[0],ti[1])
 # display(g)
 # display(f_obs)
 # display(x)
